4cd43451348475a8628b11a31c9918cf6a709f9d
marjoven <81245995@qq.com>
2019-02-20 17:51:25 +0800

    up

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -91,14 +91,17 @@ class HomeFormOrigin extends React.Component{
               </Card>  
               <Card size="small" title="售价" style={{marginTop:10}}>
                 <Row gutter={8}>
-                  {/* <Col span={12}>
-                    <FormItem label="预计售价">
-                      {getFieldDecorator('predictPrice',{
+                  <Col span={12}>
+                    <FormItem label="利润成本比（利润成本比=利润/每单总成本）">
+                      {getFieldDecorator('profitCardinal',{
                       })(
-                        <InputNumber step={0.1}/>
+                        <InputNumber step={0.1}
+                          formatter={value => `${value}%`}
+                          parser={value => value.replace('%', '')}
+                        />
                       )}
                     </FormItem>
-                  </Col> */}
+                  </Col>
                   <Col span={12}>
                     <FormItem label="配送费">
                       {getFieldDecorator('riderCost',{
@@ -127,7 +130,7 @@ class HomeFormOrigin extends React.Component{
                     <FormItem label="平台标价">
                       {getFieldDecorator('salePrice',{
                       })(
-                        <InputNumber step={0.1}/>
+                        <InputNumber disabled step={0.1}/>
                       )}
                     </FormItem>
                   </Col>
@@ -175,17 +178,7 @@ class HomeFormOrigin extends React.Component{
                       )}
                     </FormItem>
                   </Col>
-                  <Col span={12}>
-                    <FormItem label="利润成本比（利润成本比=利润/每单总成本）">
-                      {getFieldDecorator('profitCardinal',{
-                      })(
-                        <InputNumber disabled step={0.1}
-                          formatter={value => `${value}%`}
-                          parser={value => value.replace('%', '')}
-                        />
-                      )}
-                    </FormItem>
-                  </Col>
+                  
                 </Row>  
               </Card>
             </Form>
@@ -290,7 +283,7 @@ const HomeForm = Form.create({
 let calculateData=(formValues)=>{
   let {
     monthRent,predictMonthSaleCount,predictFoodCost,packageCost,
-    salePackageCost,salePrice,activityCost,riderCost,userRed
+    salePackageCost,salePrice,activityCost,riderCost,userRed,profitCardinal
   } = formValues;
   userRed=parseFloat(userRed)||0;
   salePrice=parseFloat(salePrice)||0; 
@@ -301,25 +294,47 @@ let calculateData=(formValues)=>{
   predictMonthSaleCount=parseFloat(predictMonthSaleCount)||0;
   predictFoodCost=parseFloat(predictFoodCost)||0;
   packageCost=parseFloat(packageCost)||0;
+  // =》利润成本比 = 利润 / 每单总成本  xxx% 
+  profitCardinal=parseFloat(profitCardinal)||0;
   //每单总成本 = 每月租金,水电等场地费用 / 预计月销量 + 预计食材成本 + 包装成本
   let totalCost=monthRent/predictMonthSaleCount+predictFoodCost+packageCost;
+  
+  //利润 : 每单总成本 * 利润成本比
+  let profit =  totalCost * profitCardinal/100;
+  
+  //利润 = 到账 - 每单总成本
+  // 到账 = 利润 + 每单总成本
+  // getMoney = profit + totalCost;
+  
+  let getMoney=profit + totalCost;
+  
   //平台收费： （平台标价 + 餐盒费 - 满减（等活动支出））* 0.21 
   // 当平台收费《=4.5， 
   // （平台标价 + 餐盒费 - 满减（等活动支出））* 0.21 <= 4.5
   //当 平台标价 <= 4.5/0.21 - 餐盒费 + 满减（等活动支出）,平台收费=4.5
   let appCost;
-  if(salePrice<=(4.5/0.21-salePackageCost+activityCost)){
-    appCost=4.5;
-  }else{
+  //假设平台收费为4.5，计算售价
+  salePrice=4.5/0.21+activityCost-salePackageCost;
+   
+  // =》利润 = 平台标价 + 餐盒费 - 满减（等活动支出）- 平台收费 - 每单总成本 
+  // profit=salePrice+salePackageCost-activityCost-平台收费 - totalCost
+  // profit=salePrice+salePackageCost-activityCost-平台收费 - totalCost
+  //到账 =  平台标价 + 餐盒费 - 平台收费 - 满减（等活动支出）
+  if(salePrice+salePackageCost-activityCost-4.5<getMoney){
+    //当当前售价 所得利润小于1倍成本 按照大于4.5推算售价
+    // getMoney=salePrice+salePackageCost-activityCost-(0.21*( salePrice + salePackageCost - activityCost))
+    // getMoney - salePackageCost+ activityCost + 0.21salePackageCost -0.21activityCost= 0.79*salePrice  
+    // getMoney - 0.79*salePackageCost+ 0.79*activityCost = 0.79*salePrice  
+    salePrice= (getMoney - 0.79*salePackageCost+ 0.79*activityCost)/0.79;
     appCost=0.21*( salePrice + salePackageCost - activityCost);
+  }else{
+    //当当前售价 所得利润大于1倍成本 按照小于4.5推算售价
+    // getMoney=salePrice+salePackageCost-activityCost-4.5
+    salePrice=getMoney-salePackageCost+activityCost+4.5;
+    appCost=4.5;
   }
-  //到账 :  平台标价 + 餐盒费 - 平台收费 - 满减（等活动支出）
-  let getMoney=salePrice+ salePackageCost -activityCost-appCost;
-  //利润 : 到账 - 每单总成本 
-  // =》利润 = 平台标价 + 餐盒费 - 平台收费 - 满减（等活动支出）- 每单总成本 
-  let profit =  getMoney - totalCost;
-  // =》利润成本比 = 利润 / 每单总成本
-  let profitCardinal=profit / totalCost *100
+
+  
   //客户付款： 平台标价 + 餐盒费 + 配送费 - 满减（等活动支出）
   let userPay =  salePrice + salePackageCost + riderCost - activityCost -userRed;
   return {
@@ -351,7 +366,7 @@ class Home extends React.Component{
         predictFoodCost:6,
         packageCost:1,
         totalCost:0,
-        profitCardinal:1,
+        profitCardinal:100,
         profit:0,
         predictPrice:0,
         salePrice:20,
@@ -360,14 +375,25 @@ class Home extends React.Component{
         riderCost:4,
         userRed:0
       },
-      chartData : [
+      chartDataPrice : [
         { genre: 'Sports', sold: 275 },
         { genre: 'Strategy', sold: 115},
         { genre: 'Action', sold: 120 },
         { genre: 'Shooter', sold: 350 },
         { genre: 'Other', sold: 150}
       ],
-      cols : {
+      scalePrice : {
+        sold: { alias: '销售量' },
+        genre: { alias: '游戏种类' }
+      },
+      chartDataMonthcount : [
+        { genre: 'Sports', sold: 275 },
+        { genre: 'Strategy', sold: 115},
+        { genre: 'Action', sold: 120 },
+        { genre: 'Shooter', sold: 350 },
+        { genre: 'Other', sold: 150}
+      ],
+      scaleMonthcount : {
         sold: { alias: '销售量' },
         genre: { alias: '游戏种类' }
       }
@@ -425,8 +451,8 @@ class Home extends React.Component{
       });
       console.log(transChartData);
     this.setState({
-      chartData:transChartData,
-      cols : {
+      chartDataPrice:transChartData,
+      scalePrice : {
         profit: { alias: '利润' },
         salePrice: { alias: '标价' },
         userPay:{ alias: '用户付款' },
@@ -434,11 +460,52 @@ class Home extends React.Component{
       }
     })
   }
+  generateMonthcountChart=()=>{
+    const {
+      monthRent,
+      predictFoodCost,
+      packageCost,
+      salePrice,
+      salePackageCost,
+      activityCost,
+
+    } = this.calculateFormValues;
+    let chartData=[];
+    let predictMonthSaleCount = 1;
+    //固定当前定价 对应的月销量
+    while(predictMonthSaleCount<10000){
+      let appCost;
+      if(salePrice<=(4.5/0.21-salePackageCost+activityCost)){
+        appCost=4.5;
+      }else{
+        appCost=0.21*( salePrice + salePackageCost - activityCost);
+      }
+      let totalCost=monthRent/predictMonthSaleCount+predictFoodCost+packageCost;
+      let getMoney=salePrice+ salePackageCost -activityCost-appCost;
+      let profit =  (getMoney - totalCost)*predictMonthSaleCount;
+      
+      chartData.push({
+        predictMonthSaleCount,
+        profit,
+      });
+      predictMonthSaleCount=predictMonthSaleCount+30;
+    }
+    this.setState({
+      chartDataMonthcount:chartData,
+      scaleMonthcount : {
+        profit: { alias: '利润' },
+        predictMonthSaleCount: { alias: '月销量' }
+      }
+    })
+  }
   componentDidMount(){
     this.generatePriceProfit();
   }
   render() {
-    const {formValues,chartData,cols} = this.state;
+    const {
+      formValues,chartDataPrice,scalePrice,
+      chartDataMonthcount,scaleMonthcount
+    } = this.state;
     this.calculateFormValues=calculateData(formValues);
     
     return (
@@ -450,15 +517,25 @@ class Home extends React.Component{
         {/* <pre className="language-bash">
           {JSON.stringify(formValues, null, 2)}
         </pre> */}
-        <Button onClick={this.generatePriceProfit}>生成价格-利润梯度表</Button>
-        <Chart width={500} height={500} data={chartData} scale={cols}
-          forceFit
+        <Button onClick={this.generatePriceProfit}>生成当前策略下(标价-每单利润),(标价-用户付款)关系图</Button>
+        <Chart width={500} height={500} 
+          data={chartDataPrice} scale={scalePrice}
         >
           <Axis name="salePrice" title/>
           <Axis name="amount" title/>
           <Legend position="top" dy={-20} />
           <Tooltip />
           <Geom type="line" position="salePrice*amount" color={'key'} />
+        </Chart>
+        <Button onClick={this.generateMonthcountChart}>生成当前标价下(月销量-月利润)关系图</Button>
+        <Chart width={500} height={500} 
+          data={chartDataMonthcount} scale={scaleMonthcount}
+        >
+          <Axis name="predictMonthSaleCount" title/>
+          <Axis name="profit" title/>
+          <Legend position="top" dy={-20} />
+          <Tooltip />
+          <Geom type="line" position="predictMonthSaleCount*profit"  />
         </Chart>
       </div>
     );
